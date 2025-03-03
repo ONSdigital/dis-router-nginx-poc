eac0f7bd9da66cda183f349a5bfcc24b37531acb
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2025-03-03 16:46:35 +0000

    Make webhook configurable

--- pocs/router-api/webhook/index.js
+++ pocs/router-api/webhook/index.js
@@ -1,11 +1,15 @@
 const chokidar = require("chokidar");
 const axios = require("axios");
 
+const WEB_HOOK_URL = process.env.WEB_HOOK_URL || "http://localhost:8080/webhook/redirect-config";
+
 console.log("watching database")
 
 chokidar.watch("../data/db.json").on("change", () => {
     console.log("database has changed")
-    axios.get("http://router:80/webhook/redirect-config")
+
+    console.log(`firing webhook to ${WEB_HOOK_URL}`);
+    axios.post(`${WEB_HOOK_URL}`)
         .then(() => console.log("webhook sent"))
         .catch(err => console.error("webhook error", err));
 });
